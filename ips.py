f52f297d5340cf9dc0c0a5d3b5cc5afc28517a98
Evgeniy <eugene.v.us@gmail.com>
2018-11-29 16:11:00 +0300

    first commit

---
+++ ips.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import time, socket, thread
+import ports
+
+def ping(ip):
+    alive = False
+    port = False
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.settimeout(4)
+
+    def checkPort(ip, port):
+        try:
+            exit = s.connect_ex((ip, port))
+            return (exit != 113)
+        except:
+            return False
+        finally:
+            s.close()
+
+    for port in ports.getPorts():
+        port = checkPort(ip, port)
+        if port:
+            alive = True
+            break
+    return (alive, port)
+
+def ipToList(ip, ips):
+    alive, port = ping(ip)
+    if alive:
+        ips.append(ip)
+
+def getIps():
+    ips = []
+    nw = ['172.16.2.', '192.168.224.']
+    #nw = ['192.168.224.']
+    for ip in nw:
+        for i in xrange(1, 255):
+            IP = ip + str(i)
+            thread.start_new_thread(ipToList, (IP, ips))
+    time.sleep(2)
+    return ips
+
+if __name__ == '__main__':
+    from pprint import pprint
+    IP = getIps()
+    pprint(IP)
+    print len(IP)
